40edb0ebdb226921ad9a96ffc50ba239fecda9d8
CRaez-Dev <craez.dev@gmail.com>
2024-02-17 15:02:20 -0500

    [Widgets app - Progress indicator]
    - Adding controlled Progress indicator
    - Turn into generic the app_route file

--- widgets_app/lib/config/menu/menu_items.dart
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -1,27 +1,45 @@
 import 'package:flutter/material.dart' show IconData, Icons;
+import 'package:flutter/widgets.dart';
+import 'package:widgets_app/ui/screens/screens.dart';
 
 class MenuItem {
   final String title;
   final String subTitle;
   final String link;
+  final String name;
   final IconData icon;
+  final Widget widget;
 
-  const MenuItem(
-      {required this.title,
-      required this.subTitle,
-      required this.link,
-      required this.icon});
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.name,
+    required this.icon,
+    required this.widget,
+  });
 }
 
 const appMenuItems = <MenuItem>[
   MenuItem(
+      name: ButtonsScreen.name,
       title: 'Buttons',
       subTitle: "Flutter's Buttons ",
       link: '/buttons',
-      icon: Icons.smart_button_outlined),
+      icon: Icons.smart_button_outlined,
+      widget: ButtonsScreen()),
   MenuItem(
+      name: CardsScreen.name,
       title: 'Card',
       subTitle: 'Stylized container',
       link: '/cards',
-      icon: Icons.credit_card)
+      icon: Icons.credit_card,
+      widget: CardsScreen()),
+  MenuItem(
+      name: ProgressScreen.name,
+      title: 'Progress indicator',
+      subTitle: 'Controlled and general',
+      link: '/progress_indicator',
+      icon: Icons.refresh_rounded,
+      widget: ProgressScreen())
 ];
