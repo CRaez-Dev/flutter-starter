76494d0704de4f86ccc30b21952fca80281e366d
CRaez-Dev <craez.dev@gmail.com>
2024-02-19 21:32:18 -0500

    [Cinemapedia - Clean Architecture oriented]
    - Adding new environment
    - Adding dio
    - Adding function to get movies

--- cinemapedia/lib/config/constants/environment.dart
+++ cinemapedia/lib/config/constants/environment.dart
@@ -3,4 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Environment {
   static final String theMovieDbKey =
       dotenv.env['THE_MOVIE_DB_KEY'] ?? 'THE_MOVIE_DB_KEY not found!';
+  static final String theMovieDbBaseUrl =
+      dotenv.env['THE_MOVIE_DB_BASE_URL'] ?? 'THE_MOVIE_DB_BASE_URL not found!';
+      
 }
