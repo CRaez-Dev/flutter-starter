d60ca3039ac0affba20f03940b0c2fca333a7263
CRaez-Dev <craez.dev@gmail.com>
2024-02-19 22:06:28 -0500

    [Cinemapedia - Clean Architecture oriented]
    - Adding mapper function moviedb

--- cinemapedia/lib/config/constants/environment.dart
+++ cinemapedia/lib/config/constants/environment.dart
@@ -5,5 +5,8 @@ class Environment {
       dotenv.env['THE_MOVIE_DB_KEY'] ?? 'THE_MOVIE_DB_KEY not found!';
   static final String theMovieDbBaseUrl =
       dotenv.env['THE_MOVIE_DB_BASE_URL'] ?? 'THE_MOVIE_DB_BASE_URL not found!';
-      
+
+  static final String theMovieDbImageUrl =
+      dotenv.env['THE_MOVIE_DB_IMAGE_URL'] ??
+          'THE_MOVIE_DB_IMAGE_URL not found!';
 }
